c34fe4e49fc46b84304ac7b4f8445b60af53b23e
raajsupramanian <raajsupramanian@gmail.com>
2016-03-28 21:45:01 +0800

    initial values

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from yahoo_finance import Share
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+class ShAre(Resource):
+    def get(self, shareid):
+	ad = Share(shareid)
+	return {'open_price': ad.get_open(), 'price': ad.get_price(), 'change':ad.get_change()}
+	
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(ShAre, '/<string:shareid>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
