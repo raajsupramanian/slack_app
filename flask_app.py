f56cb5b34e07f895910bf9fe4ddd156df1285a19
Raajsupra <raajsupramanian.muralidharan@autodesk.com>
2016-03-29 15:20:46 +0800

    added custom data

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from flask_restful import Resource, Api
 from gfin import getQuotes
 import json
@@ -7,14 +8,16 @@ app = Flask(__name__)
 api = Api(app)
 
 class ShAre(Resource):
-    def get(self, symbol=None):
+    def get(self):
+	symbol = request.args.get('text')
+	if not symbol:
+		symbol = "ADSK"
 	try:
 		resp = getQuotes([symbol])
 		return resp[0]
 	except:
 		return "No Data for given code"
-api.add_resource(ShAre, '/share/<symbol>', defaults={'symbol': 'ADSK'}, endpoint='GET with symbol')
-api.add_resource(ShAre, '/share/', defaults={'symbol': 'ADSK'}, endpoint='GET default')
+api.add_resource(ShAre, '/share', endpoint='GET default')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5002, host="0.0.0.0")
