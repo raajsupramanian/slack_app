05e73494cb4f8c37da9d540d906bc0a01ded1471
Raajsupra <raajsupramanian.muralidharan@autodesk.com>
2016-03-29 18:50:58 +0800

    only data

--- data.sql
+++ data.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `bus_stops`
 --
 
+use raajsupra$lta;
+
 DROP TABLE IF EXISTS `bus_stops`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
