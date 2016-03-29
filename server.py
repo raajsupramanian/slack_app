3a8cb234c0ea186f75dd513c18d421612451e013
raajsupramanian <raajsupramanian@gmail.com>
2016-03-29 11:05:40 +0800

    fin

--- server.py
+++ server.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask_restful import Resource, Api
-from yahoo_finance import Share
+from gfin import getQuotes
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
 class ShAre(Resource):
-    def get(self):
-	return "Hi"
-	
-
-api.add_resource(ShAre, '/')
+    def get(self, symbol):
+	try:
+		resp = getQuotes([symbol])
+		return resp[0]
+	except:
+		return "No Data for given code"
+api.add_resource(ShAre, '/share/<string:symbol>')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
